27ef3028c3b332d2bd09ca08fc69a9f01d0cd678
Joey Wong <jjay9225@gmail.com>
2017-12-09 22:11:39 -0500

    Added webpack.config.js module rules and .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,12 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
+    },
+    module:{
+        rules: [{
+            leader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
