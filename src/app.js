416a6c68de5f0fab738073fa554951e72a2d396e
Joey Wong <jjay9225@gmail.com>
2017-12-10 10:55:19 -0500

    Separated constructors for class declarations with new babel transform-class-properties functions

--- src/app.js
+++ src/app.js
@@ -3,4 +3,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render( < IndecisionApp / > , document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Mike';
+        this.getGreeting = this.getGreeting.bind(this);
+    }
+
+    getGreeting() {
+        return `Hi, my name is ${this.name}`;
+    }
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(oldSyntax);
+console.log(getGreeting());
+
+//New syntax
+//class property syntax from transform-class-properties babel plugin removes the need to bind methods.
+class NewSyntax {
+    name = 'Jen';
+    getGreeting = () => {
+        return `Hi, my name is ${this.name}`;
+    }
+}
+
+const newSyntax = new NewSyntax();
+const newGreeting = newSyntax.getGreeting;
+console.log(newSyntax);
+console.log(newGreeting());
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,91 +5,106 @@ import Options from './Options';
 import AddOptions from './AddOptions';
 
 export default class IndecisionApp extends React.Component {
-    
-        constructor(props){
-            super(props);
-            this.handleDeleteOption = this.handleDeleteOption.bind(this);
-            this.handlePick = this.handlePick.bind(this);
-            this.handleAddOption = this.handleAddOption.bind(this);
-            this.handleDeleteOptionSingle = this.handleDeleteOptionSingle.bind(this);
-            this.state = {
-                options: [],
-            };
+    state = {
+        options: [],
+    };
+
+    handleDeleteOption = (option) => {
+        this.setState(() => ({
+            options: [],
+        }));
+    };
+
+    handleDeleteOptionSingle = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => option !== optionToRemove)
+        }));
+    }
+
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
         }
-    
-        //only class-based components can call the mount methods, stateless functional components can't
-        //which is a tradeoff for speed.
-        componentDidMount(){
-    
-            try{
-                const json = localStorage.getItem('options');
-                const options = JSON.parse(json);
-                this.setState(()=> ({ options }));
-                console.log('Component did mount');
-            } catch(e){
-    
-            }
-      
-        }
-    
-        componentDidUpdate(prevProps, prevState){
-            if (prevState.options.length !== this.state.options.length){
-                console.log('Component did update');
-                const json = JSON.stringify(this.state.options);
-                localStorage.setItem('options', json);
-            }
-        }
-    
-        //this component is barely used
-        componentWillUnmount(){
-            console.log('ComponentWillUnmount');
-        }
-    
-        handlePick(){
-            const randomNumber = Math.floor(Math.random() * this.state.options.length);
-            alert(this.state.options[randomNumber]);
-        }
-    
-        handleAddOption(option){
-    
-            if(!option) {
-                return 'Enter valid value to add item';
-            } else if(this.state.options.indexOf(option) > -1){
-                return 'This option already exists';
-            }
-    
-            this.setState((prevState)=>({
-                options: prevState.options.concat(option)
+
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    }
+
+    handlePick = () => {
+        const randomNumber = Math.floor(Math.random() * this.state.options.length);
+        alert(this.state.options[randomNumber]);
+    }
+
+    //only class-based components can call the mount methods, stateless functional components can't
+    //which is a tradeoff for speed.
+    componentDidMount() {
+
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            this.setState(() => ({
+                options
             }));
+            console.log('Component did mount');
+        } catch (e) {
+
         }
-    
-        handleDeleteOption(){
-            this.setState(()=>({
-                    options : [],
-            }));
-        }
-    
-        handleDeleteOptionSingle(optionToRemove){
-            this.setState((prevState)=>({
-                options: prevState.options.filter((option)=>option !== optionToRemove)
-            }));
-        }
-    
-        render(){
-    
-            const title = 'Indecision!';
-            const subtitle = 'Put your life in the hands of a computer!';
-    
-            return (
-            <div>
-                <Header title={title} subtitle={subtitle}/>
-                <Action hasOptions={this.state.options.length > 0}
-                handlePick={this.handlePick}/>
-                <Options options={this.state.options} 
-                handleDeleteOption={this.handleDeleteOption}
-                handleDeleteOptionSingle={this.handleDeleteOptionSingle}/>
-                <AddOptions handleAddOption={this.handleAddOption}/>
-            </div>
-            );
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            console.log('Component did update');
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
         }
     }
+
+    //this component is barely used
+    componentWillUnmount() {
+        console.log('ComponentWillUnmount');
+    }
+
+    render() {
+
+        const title = 'Indecision!';
+        const subtitle = 'Put your life in the hands of a computer!';
+
+        return ( <
+            div >
+            <
+            Header title = {
+                title
+            }
+            subtitle = {
+                subtitle
+            }
+            /> <
+            Action hasOptions = {
+                this.state.options.length > 0
+            }
+            handlePick = {
+                this.handlePick
+            }
+            /> <
+            Options options = {
+                this.state.options
+            }
+            handleDeleteOption = {
+                this.handleDeleteOption
+            }
+            handleDeleteOptionSingle = {
+                this.handleDeleteOptionSingle
+            }
+            /> <
+            AddOptions handleAddOption = {
+                this.handleAddOption
+            }
+            /> <
+            /div>
+        );
+    }
+}
