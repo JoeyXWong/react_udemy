91cd01c0f3cea8db3ed249cd1a7cc86796d89c0c
Joey Wong <jjay9225@gmail.com>
2017-12-09 22:13:34 -0500

    Fixed typo for loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     module:{
         rules: [{
-            leader: 'babel-loader',
+            loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
         }]
