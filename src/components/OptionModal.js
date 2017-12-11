c64e33a91c0718f858dc5a171a531d620e1fb1bd
Joey Wong <jjay9225@gmail.com>
2017-12-11 07:50:34 -0500

    Styled the modal and handled the wordbreak to format correctly

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,10 +5,12 @@ const OptionModal = (props) => (
     <Modal 
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
-    onRequestClose={props.handleRemoveSelectedState}>
-        <h3>Selected Option</h3>
-        <h3>{props.selectedOption && <p>{props.selectedOption}</p>}</h3>
-        <button onClick={props.handleRemoveSelectedState}>Return</button>
+    onRequestClose={props.handleRemoveSelectedState}
+    closeTimeoutMS={200}
+    className="modal">
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleRemoveSelectedState}>Return</button>
     </Modal>
 );
 
